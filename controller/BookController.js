3c240d5a162f36b830eec1c131b719ef41e03031
Minkyung Kong <108920053+alsrud991026@users.noreply.github.com>
2024-01-04 18:44:22 +0900

    Feat: 개별 도서 조회 기능 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -17,7 +17,28 @@ const allBooks = (req, res) => {
     });
 };
 
-const bookDetail = (req, res) => {};
+const bookDetail = (req, res) => {
+    const { id } = req.params;
+    const sql = 'select * from books where id = ?';
+
+    conn.query(sql, id, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                error: err.message,
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: '해당하는 도서가 없습니다.',
+            });
+        }
+
+        return res.status(StatusCodes.OK).json({
+            book: results[0],
+        });
+    });
+};
 
 const booksByCategory = (req, res) => {};
 
