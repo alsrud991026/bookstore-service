78a9e5c41ab04c8d870de7b00a7e8f0cb03abea7
Minkyung Kong <108920053+alsrud991026@users.noreply.github.com>
2024-01-05 22:14:10 +0900

    Refactor: 전체 도서 조회와 카테고리별 도서 조회 함수 리팩토링

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,44 +3,28 @@ const { StatusCodes } = require('http-status-codes');
 
 const allBooks = (req, res) => {
     const { category_id } = req.query;
+    let sql = 'select * from books';
 
     if (category_id) {
-        const sql = 'select * from books where category_id = ?';
-
-        conn.query(sql, category_id, (err, results) => {
-            if (err) {
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    error: err.message,
-                });
-            }
-
-            if (results.length === 0) {
-                return res.status(StatusCodes.NOT_FOUND).json({
-                    message: '해당하는 카테고리의 도서가 없습니다.',
-                });
-            }
-
-            return res.status(StatusCodes.OK).json(results);
-        });
-    } else {
-        const sql = 'select * from books';
-
-        conn.query(sql, (err, results) => {
-            if (err) {
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    error: err.message,
-                });
-            }
-
-            if (results.length === 0) {
-                return res.status(StatusCodes.NOT_FOUND).json({
-                    message: '도서가 없습니다.',
-                });
-            }
-
-            return res.status(StatusCodes.OK).json(results);
-        });
+        sql += ' where category_id = ?';
     }
+
+    conn.query(sql, category_id, (err, results) => {
+        if (err) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                error: '서버 에러',
+            });
+        }
+
+        if (results.length === 0) {
+            const message = category_id ? '해당하는 카테고리의 도서가 없습니다.' : '도서가 없습니다.';
+            return res.status(StatusCodes.NOT_FOUND).json({
+                message: message,
+            });
+        }
+
+        return res.status(StatusCodes.OK).json(results);
+    });
 };
 
 const bookDetail = (req, res) => {
