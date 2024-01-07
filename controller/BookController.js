6f6a288d88d125f62d6a19290aad1b5d70bd5912
Minkyung Kong <108920053+alsrud991026@users.noreply.github.com>
2024-01-07 22:23:31 +0900

    Feat: 도서 목록 조회 페이징 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,24 +2,43 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
 const allBooks = (req, res) => {
-    const { category_id, news } = req.query;
-    let values = [];
+    const { category_id, news, limit, current_page } = req.query;
+    // limit : 페이지 당 도서 수
+    // currentPage : 현재 페이지
+    // offset : 페이지 당 도서 수 * (현재 페이지 - 1)
+
+    const parsedLimit = parseInt(limit);
+    const parsedCurrentPage = parseInt(current_page);
+
+    if (!parsedLimit || !parsedCurrentPage || parsedLimit < 1 || parsedCurrentPage < 1) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            error: 'limit 혹은 currentPage가 잘못되었습니다.',
+        });
+    }
+
+    const offset = parsedLimit * (parsedCurrentPage - 1);
+    const values = [];
+
     let sql = 'select * from books';
 
     if (category_id && news) {
         sql +=
             ' left join category on books.category_id = category.id where category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-        values = [category_id, news];
+        values.push(category_id, news);
     } else if (category_id) {
         sql += ' left join category on books.category_id = category.id where category_id = ?';
-        values = [category_id];
+        values.push(category_id);
     } else if (news) {
         sql += ' where pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-        values = [news];
+        values.push(news);
     }
+
+    sql += ' limit ?, ?';
+    values.push(offset, parsedLimit);
 
     conn.query(sql, values, (err, results) => {
         if (err) {
+            console.log(err);
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
                 error: '서버 에러',
             });
