4770fb6ee0a807152bc3f9aebe542de21de522db
Minkyung Kong <108920053+alsrud991026@users.noreply.github.com>
2024-01-08 21:46:10 +0900

    Refactor: CategoryController 비동기 처리로 수정

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,24 +1,28 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
-const allCategory = (req, res) => {
+const allCategory = async (req, res) => {
+    const connection = await conn.getConnection();
     const sql = 'select * from category';
 
-    conn.query(sql, (err, results) => {
-        if (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                error: '서버 에러',
-            });
-        }
+    try {
+        const [rows] = await connection.query(sql);
 
-        if (results.length === 0) {
+        if (rows.length === 0) {
             return res.status(StatusCodes.NOT_FOUND).json({
                 message: '카테고리가 없습니다.',
             });
         }
 
-        return res.status(StatusCodes.OK).json(results);
-    });
+        return res.status(StatusCodes.OK).json(rows);
+    } catch (err) {
+        console.log(err);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: '카테고리 조회 중 에러가 발생하였습니다.',
+        });
+    } finally {
+        connection.release();
+    }
 };
 
 module.exports = { allCategory };
