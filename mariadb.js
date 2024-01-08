d97170fb9538069b1f9f58a77fddd70bb81a1be9
Minkyung Kong <108920053+alsrud991026@users.noreply.github.com>
2024-01-08 18:48:01 +0900

    Refactor: 데이터베이스 pool 수정

--- mariadb.js
+++ mariadb.js
@@ -1,12 +1,10 @@
-// get the client
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// create the connection to database
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
 });
 
-module.exports = connection;
+module.exports = pool;
