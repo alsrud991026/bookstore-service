0c3f941a3f9575d16441e455f4f10adf3ac74bb0
Minkyung Kong <108920053+alsrud991026@users.noreply.github.com>
2024-01-10 19:57:15 +0900

    Feat: 장바구니 유효성 검사 추가

--- routes/carts.js
+++ routes/carts.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { addToCart, getCartItems } = require('../controller/CartController');
+const { addToCart, getCartItems, deleteCartItem } = require('../controller/CartController');
+const { validatesAddToCart, validatesGetCartItems } = require('../middleware/CartMiddleware');
 
 router.use(express.json());
 
 // 장바구니 담기
 router
     .route('/')
-    .post(addToCart)
+    .post(validatesAddToCart, addToCart)
     // 장바구니 조회
-    .get(getCartItems);
+    .get(validatesGetCartItems, getCartItems);
 
 // 장바구니 도서 삭제
-router.delete('/:id', (req, res) => {
-    res.json({
-        message: '장바구니 도서 삭제',
-    });
-});
+router.delete(deleteCartItem);
 
 // // 장바구니에서 선택한 주문 예상 상품 목록 조회
 // router.get('/', (req, res) => {
